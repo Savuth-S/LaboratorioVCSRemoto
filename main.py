35997a1964749baf2fa4e85e9e33e9a4c9c111c9
Savuth-S <SavuthS4ns@protonmail.com>
2020-03-26 20:01:01 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ Created on Thu Mar 26 18:01:05 2020
 @author: Savuth S.
 """
 
-a = int(input("Ingrese el primer numero de entrada: "))
-c = int(input("Ingrese el segundo numero de entrada: "))
+#%%Producto de dos numeros
+a = float(input("Ingrese el primer numero de entrada: "))
+b = float(input("Ingrese el segundo numero de entrada: "))
 
 print("El producto de "+ str(a) +" y "+ str(c) +" es: "+ str(a*c))
 print("El doble de "+ str(a) +" es: "+ str(a*2))
+        
+#%%Cuadrado de un numero
+c = int(input("Ingrese un numero entero de entrada: "))
+print("El cuadrado de "+ str(b))
+
+#%%Raiz cuadrada de un numero
+d = float(input("Ingrese un numero de entrada: "))
+
