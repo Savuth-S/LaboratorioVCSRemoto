fa440fa68976ae85e6620ff853c1e40243b9ff87
Savuth-S <SavuthS4ns@protonmail.com>
2020-03-26 19:11:30 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 18:01:05 2020
+
+@author: Savuth S.
+"""
+
+a = int(input("Ingrese el primer numero de entrada: "))
+c = int(input("Ingrese el segundo numero de entrada: "))
+
+print("El producto de "+ str(a) +" y "+ str(c) +" es: "+ str(a*c))
+print("El doble de "+ str(a) +" es: "+ str(a*2))
